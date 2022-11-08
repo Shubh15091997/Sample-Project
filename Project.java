ac2e1ad9fc1822b367557f041ad5f5373670fdc5
Shubh <Shubhamgaikwad496@gmail.com>
2022-11-08 12:45:38 +0530

    update

--- Project.java
+++ Project.java
@@ -1,7 +1,15 @@
 public class project{
+<<<<<<< Updated upstream
 publiv void  add(){
 
 system.out.println("hello");
+=======
+public String void main(String[] args){
+int a=10;
+int b=20;
+int c= a+b;
+system.out.println(c);
+>>>>>>> Stashed changes
 
 }
 
